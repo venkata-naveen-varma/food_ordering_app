2430c183acdfc918c0b54abafdf1007c520a345d
venkata-naveen-varma <venkata2naveen@gmail.com>
2023-12-18 16:32:19 -0500

    routes

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-
+import AdminRoute from "./routes/AdminRoute";
+import VandorRoute from "./routes/VandorRoute";
 
 const app = express();
 
-app.use("/", (req, res)=>{
-    return res.json("Hello");
-});
+app.use("/admin", AdminRoute);
+app.use("/vandor", VandorRoute);
 
 
 app.listen(8000, ()=>{
--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,6 @@
-import * from "./AdminRoute";
-import * from "./CustomerRoute";
-import * from "./DeliveryRoute";
-import * from "./ShoppingRoutes";
-import * from "./VandorRoute";
+import AdminRoute from "./AdminRoute";
+import VandorRoute from "./VandorRoute";
+import CustomerRoute from "./CustomerRoute";
+import DeliveryRoute from "./DeliveryRoute";
+import ShoppingRoutes from "./ShoppingRoutes";
 
